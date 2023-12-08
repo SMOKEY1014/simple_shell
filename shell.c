456a4512bb94afadabd4cf6a668bd6d9e169a0a0
SMOKEY1014 <smokeydhlamini@gmail.com>
2023-12-08 16:53:11 +0200

    compiling shell

---
+++ shell.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+
+int main(void)
+{
+	
+	char *input = NULL;
+
+	while (1)
+	{
+		printf(PROMPT);
+
+		input_tkn(input);
+		
+	}
+	free(input); /* Free allocated memory */
+	return (0);
+}
